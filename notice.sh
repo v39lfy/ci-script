731d590953f4cc54c38c7f85e08771120da46690
v39lfy <admin@onlywish.me>
2021-01-28 18:53:37 +0800

    修改脚本适配sh

--- notice.sh
+++ notice.sh
@@ -13,8 +13,24 @@ echo $BRANCH_NAME
 # 当前最后两次push的reflog
 LINES=$(git reflog show $BRANCH_NAME | grep "update by push" | awk '{print $1}' | head -n 2)
 
-# 转换成数组
-ARR=($LINES)
+# 转换成数组, 获取开始和结束的标签
+i=0
+for line in ${LINES}; do
+	eval ARR$i=$line
+	i=$((i+1))
+done
+
+if [ $ARR0 ]; then
+	END_SEGMENT=$ARR0
+fi
+
+if [ $ARR1 ]; then
+	BEGIN_SEGMENT=$ARR1
+else
+	BEGIN_SEGMENT=$ARR0
+fi
+
+
 
 # 填充日期格式设置的值
 if [ ! -n "$DATE_FORMAT" ]; then
@@ -23,7 +39,7 @@ fi
 echo $DATE_FORMAT
 
 # 获取两次push区间内的所有的提交记录
-commits=`git log --abbrev-commit --date=format:"$DATE_FORMAT" --pretty="%cd %an: %B" ${ARR[${#ARR[@]}-1]}..${ARR[0]}`
+commits=`git log --abbrev-commit --date=format:"$DATE_FORMAT" --pretty="%cd %an: %B" ${BEGIN_SEGMENT}..${END_SEGMENT}`
 message=`echo  "${commits//$'\n'/\n}"`
 
 # 填充消息标题
