00d4d0f7b083adc785ba40e21cf9a82cc093f806
v39lfy <admin@onlywish.me>
2021-01-28 18:01:27 +0800

    获取CI分支名

--- notice.sh
+++ notice.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # 获取当前分支名
-BRANCH_NAME=origin/$(git symbolic-ref --short -q HEAD)
+BRANCH_NAME=''
+if [ ! -n $CI_COMMIT_BRANCH ]; then
+	BRANCH_NAME=$CI_COMMIT_BRANCH
+	echo $BRANCH_NAME
+else
+	BRANCH_NAME=origin/$(git symbolic-ref --short -q HEAD)
+fi
+echo $BRANCH_NAME
+
 # 当前最后两次push的reflog
-LINES=$(git reflog show $BRANCH_NAME | grep "update by push" | awk '{print $1}' | head -n 2)
+LINES=$(git reflog show  | grep "update by push" | awk '{print $1}' | head -n 2)
 
 # 转换成数组
 ARR=($LINES)
@@ -25,7 +33,7 @@ fi
 
 # 钉钉推送
 if [ $DING_BOT_TOKEN ]; then
-	body=$(echo -e '{"msgtype": "text","text": {"content": "'$TITLE'\n' $message '\n"}}')
+	body=$(echo '{"msgtype": "text","text": {"content": "'$TITLE'\n' $message '\n"}}')
 	echo $body
 	curl 'https://oapi.dingtalk.com/robot/send?access_token='$DING_BOT_TOKEN \
 	       -H 'Content-Type: application/json' \
