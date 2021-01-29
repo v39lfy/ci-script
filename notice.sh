bc3cb9354a68ed04314f2654a7698f7977dc14de
v39lfy <admin@onlywish.me>
2021-01-29 11:42:30 +0800

    剔除空格

--- notice.sh
+++ notice.sh
@@ -51,8 +51,9 @@ fi
 # 获取两次push区间内的所有的提交记录
 commits=`git log --abbrev-commit --date=format:"$DATE_FORMAT" --pretty="%cd %an: %B" ${BEGIN_SEGMENT}..${END_SEGMENT}`
 echo $commits
-message=`echo  "${commits//$'\n'/\\n}"`
 
+message=`echo  "${commits//$'\n\n'/\n}"`
+message=`echo  "${message//$'\n'/\\n}"`
 echo $message
 # 填充消息标题
 if [ ! -n "$TITLE" ]; then
