27658b23f189255a2fe60c832d2e8183058cae35
bifan <five1987@gmail.com>
2021-01-30 12:28:01 +0800

    尝试使用<br/> 换行

--- notice.sh
+++ notice.sh
@@ -50,12 +50,12 @@ fi
 
 # git log 格式
 # %cd 时间
-PRETTY_FORMAT="> - %an: "
+PRETTY_FORMAT="> %an: "
 # 在CI状态，添加提交链接
 if [ $CI_PROJECT_URL ]; then
-	PRETTY_FORMAT=$PRETTY_FORMAT"[%B]("$CI_PROJECT_URL"/-/commit/%H) \n"
+	PRETTY_FORMAT=$PRETTY_FORMAT"[%B]("$CI_PROJECT_URL"/-/commit/%H)<br/>"
 else
-	PRETTY_FORMAT=$PRETTY_FORMAT"%B \n"
+	PRETTY_FORMAT=$PRETTY_FORMAT"%B<br/>"
 fi
 
 # 获取两次push区间内的所有的提交记录
