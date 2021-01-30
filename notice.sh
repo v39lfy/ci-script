87f75d43b9b66b741ac24867fcff23882069755e
bifan <five1987@gmail.com>
2021-01-30 13:03:56 +0800

    测试钉钉官方demo

--- notice.sh
+++ notice.sh
@@ -73,6 +73,8 @@ if [ ! -n "$TITLE" ]; then
 	TITLE='code is updated:'
 fi
 
+#test
+commits = "#### 杭州天气 @150XXXXXXXX \n> 9度，西北风1级，空气良89，相对温度73%\n> ![screenshot](https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png)\n> ###### 10点20分发布 [天气](https://www.dingalk.com) \n"
 
 # 钉钉推送
 if [ $DING_BOT_TOKEN ]; then
