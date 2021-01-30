21dd436de484961be02ce8acea77e56a312855d2
bifan <five1987@gmail.com>
2021-01-30 12:32:34 +0800

    尝试<br/> 换行，加点空格

--- notice.sh
+++ notice.sh
@@ -53,9 +53,9 @@ fi
 PRETTY_FORMAT="> %an: "
 # 在CI状态，添加提交链接
 if [ $CI_PROJECT_URL ]; then
-	PRETTY_FORMAT=$PRETTY_FORMAT"[%B]("$CI_PROJECT_URL"/-/commit/%H)<br/>"
+	PRETTY_FORMAT=$PRETTY_FORMAT"[%B]("$CI_PROJECT_URL"/-/commit/%H)  <br/>  "
 else
-	PRETTY_FORMAT=$PRETTY_FORMAT"%B<br/>"
+	PRETTY_FORMAT=$PRETTY_FORMAT"%B  <br/>  "
 fi
 
 # 获取两次push区间内的所有的提交记录
